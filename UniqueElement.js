b22f746cdc5f24befe03eac2bc5e48c1a0a04e89
Kiren Paul <kiren.paul@mobme.in>
2018-01-13 13:12:23 +0530

    find unique elemets in an array algorithm in js

---
+++ UniqueElement.js
@@ -0,0 +1,17 @@
+// Write a function to return an element that appears only once in a given array.
+// Input: [10, 1, 10, 3, 10, 1, 1, 2, 3, 3,69]
+// Output: 2,69
+
+// Please note that the function should be a javascript code
+// let myArray = [10, 1, 10, 3, 10, 1, 1, 2, 3, 3,6];
+function getUniqueElementsFromArray(myArray) {
+  let uniqueArray = [];
+  myArray.forEach(function(item, index) {
+    let intermediateArray = myArray.slice();
+    if (intermediateArray.splice(index, 1).indexOf(item) === -1)
+      uniqueArray.push(item);
+  });
+  console.log("Output", uniqueArray);
+  return uniqueArray;
+}
+getUniqueElementsFromArray([10, 1, 10, 3, 10, 1, 1, 2, 3, 3, 69]);
