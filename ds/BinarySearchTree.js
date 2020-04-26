0e69b16fe62a0c7464e58792d516b54a4e1e331d
PaulKiren <kirenpaul@gmail.com>
2020-04-27 01:11:53 +0700

    insert and contains functions added

--- ds/BinarySearchTree.js
+++ ds/BinarySearchTree.js
@@ -1,5 +1,47 @@
-function BinarySearchTree(value) {
+function BST(value) {
     this.value = value;
     this.left = null;
     this.right = null;
 }
+
+
+// insert new value to the tree
+//                  50
+//            30           60
+//         20    45     55     70
+//      10     35         58      105
+
+BST.prototype.insert = function (value) {
+    if (value <= this.value) {
+        if (!this.left) this.left = new BST(value);
+        else this.left.insert(value);
+    } else {
+        if (!this.right) this.right = new BST(value);
+        else this.right.insert(value);
+    }
+}
+
+BST.prototype.contains = function (value) {
+    if(this.value === value) return true;
+
+    if(value< this.value) {
+        if(!this.left) return false;
+        else return this.left.contains(value);
+    }
+    else {
+        if(!this.right) return false;
+        else return this.right.contains(value);
+
+    }
+
+}
+const bTree = new BST(50);
+bTree.insert(30);
+bTree.insert(60);
+bTree.insert(45);
+bTree.insert(35);
+bTree.insert(20);
+bTree.insert(10);
+
+
+console.log(bTree.contains(10));
