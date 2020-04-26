e92cdcaa9dac9fb9a9ba825d7c30c0e4c63e65f7
PaulKiren <kirenpaul@gmail.com>
2020-04-26 18:39:55 +0700

    add head fn added

--- ds/linkedlist.js
+++ ds/linkedlist.js
@@ -3,8 +3,21 @@ function LinkedList() {
     this.tail = null;
 
 }
-function Node(value, next, previous) {
+function Node(value, next, prev) {
     this.value = value;
     this.next = next;
-    this.previous = previous;
+    this.prev = prev;
 }
+
+LinkedList.prototype.addToHead=function(value) {
+    const newNode = new Node(value, this.head, null);
+    if (this.head) this.head.prev = newNode;
+    else this.tail = newNode;
+    this.head = newNode;
+}
+
+const LL = new LinkedList();
+LL.addToHead(100);
+LL.addToHead(200);
+LL.addToHead(300);
+console.log(LL);
