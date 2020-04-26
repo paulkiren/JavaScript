273007addaf5b10521d4e8dd9213917fa6a92b8d
PaulKiren <kirenpaul@gmail.com>
2020-04-26 18:44:40 +0700

    add to tail added

--- ds/linkedlist.js
+++ ds/linkedlist.js
@@ -16,8 +16,17 @@ LinkedList.prototype.addToHead=function(value) {
     this.head = newNode;
 }
 
+LinkedList.prototype.addToTail = function (value) {
+    const newNode = new Node(value, null, this.tail);
+    if(this.tail) this.tail.next = newNode;
+    else this.head = newNode;
+    this.tail = newNode;
+}
+
 const LL = new LinkedList();
 LL.addToHead(100);
 LL.addToHead(200);
 LL.addToHead(300);
+LL.addToTail(80);
+LL.addToTail(60);
 console.log(LL);
