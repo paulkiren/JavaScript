12c081f520eb756e4442986fc1de6c3111a4a87c
PaulKiren <kirenpaul@gmail.com>
2020-04-28 20:15:27 +0700

    one more method added

--- ds/Palindrome.js
+++ ds/Palindrome.js
@@ -4,4 +4,17 @@ function isPalindrome(input) {
     return (input === input.split('').reverse().join(''));
 }
 
-console.log(isPalindrome("t malayalam t"))
+function isPalindrome_one(input) {
+    input = input.toLowerCase();
+    let charArray = input.split('');
+    let permittedCharsArray = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    let lettersArray = [];
+    charArray.forEach(e => {
+        if(permittedCharsArray.indexOf(e)>-1) {
+            lettersArray.push(e);
+        }
+    });
+ return lettersArray.join('') === lettersArray.reverse().join('') ;
+}
+
+console.log(isPalindrome_one("t malayalam t"))
