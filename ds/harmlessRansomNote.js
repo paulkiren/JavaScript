12577b71e506af7d37fa67b5163f656b35f5f3bb
PaulKiren <kirenpaul@gmail.com>
2020-04-28 16:26:21 +0700

    big o check for  harmlessRansomNote

--- ds/harmlessRansomNote.js
+++ ds/harmlessRansomNote.js
@@ -2,11 +2,13 @@
 function harmlessRansomNote(noteText, magazineText) {
     let notesArray = noteText.split(' '), magazineArray = magazineText.split(' ');
     let magazineWords = {};
+     // Big O(n);
     magazineArray.forEach(word => {
         if (!magazineWords[word]) magazineWords[word] = 0;
         magazineWords[word] += 1;
     });
     let isAvalidNote = true;
+    // Big O(m);
     notesArray.forEach(note_word => {
         if (magazineWords[note_word]) {
             magazineWords[note_word] -= 1;
@@ -18,4 +20,6 @@ function harmlessRansomNote(noteText, magazineText) {
     return isAvalidNote;
 
 }
+
+// Time complexity O(n + m);
 harmlessRansomNote('this is a secret note for you from a secret admirer', 'es as they are far from populated areas but it is worth the effort a tip i have for you is to go ');
