27eb0e9d02d854e3e328d549e729f099754f4e48
PaulKiren <kirenpaul@gmail.com>
2020-05-04 12:21:03 +0700

    test_some_js

---
+++ ds/bindCallandApply.js
@@ -0,0 +1,16 @@
+//bind Call and Apply
+
+const car = {
+
+}
+
+console.log(900.9 === 300.3 * 3); 
+
+const a = {};
+// const b = { name: 'b' };
+const c = [1, 'c' ];
+ 
+// a[b] = 200;
+a[c] = 400;
+ 
+console.log(JSON.stringify(a));
