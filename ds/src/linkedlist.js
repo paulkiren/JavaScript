56321c4a7b83855128272483158f64481fb300dc
Kiren <kiren@azurethailand.com>
2020-05-26 14:25:43 +0700

    adding test js

--- ds/src/linkedlist.js
+++ ds/src/linkedlist.js
@@ -1,71 +1,85 @@
-function LinkedList() {
-    this.head = null;
-    this.tail = null;
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
 
-}
-function Node(value, next, prev) {
-    this.value = value;
-    this.next = next;
-    this.prev = prev;
-}
-
-LinkedList.prototype.addToHead = function (value) {
-    const newNode = new Node(value, this.head, null);
-    if (this.head) this.head.prev = newNode;
-    else this.tail = newNode;
-    this.head = newNode;
-}
-
-LinkedList.prototype.addToTail = function (value) {
-    const newNode = new Node(value, null, this.tail);
-    if (this.tail) this.tail.next = newNode;
-    else this.head = newNode;
-    this.tail = newNode;
-}
-
-LinkedList.prototype.deleteHead = function () {
-    if (!this.head) return null;
-    const val = this.head.value;
-    this.head = this.head.next;
-    if (this.head) this.head.prev = null;
-    else this.tail = null;
-    return val;
-}
-
-LinkedList.prototype.deleteTail = function () {
-    if (!this.tail) return null;
-    const val = this.tail.value;
-    this.tail = this.tail.prev;
-    if (this.tail) this.tail.next = null;
-    else this.head = null;
-    return val;
-}
-
-LinkedList.prototype.search = function (searchValue) {
-    let currentNode = this.head;
-    while (currentNode) {
-        if (currentNode.value === searchValue) return currentNode.value;
-        currentNode = currentNode.next;
     }
-
-}
-// Assignment Index Of
-// list 3 40 3 7
-// indexOf(3) >  0, 2
-
-LinkedList.prototype.indexOf = function (value) {
-    let output = [], index = 0, currentNode = this.head;
-    while (currentNode) {
-
-        if (currentNode.value === value)
-            output.push(index);
-        index++;
-        currentNode = currentNode.next;
+    addToHead(value) {
+        const newNode = new Node(value, this.head, null);
+        if (this.head)
+            this.head.prev = newNode;
+        else
+            this.tail = newNode;
+        this.head = newNode;
     }
- return output;
+    addToTail(value) {
+        const newNode = new Node(value, null, this.tail);
+        if (this.tail)
+            this.tail.next = newNode;
+        else
+            this.head = newNode;
+        this.tail = newNode;
+    }
+    deleteHead() {
+        if (!this.head)
+            return null;
+        const val = this.head.value;
+        this.head = this.head.next;
+        if (this.head)
+            this.head.prev = null;
+        else
+            this.tail = null;
+        return val;
+    }
+    deleteTail() {
+        if (!this.tail)
+            return null;
+        const val = this.tail.value;
+        this.tail = this.tail.prev;
+        if (this.tail)
+            this.tail.next = null;
+        else
+            this.head = null;
+        return val;
+    }
+    search(searchValue) {
+        let currentNode = this.head;
+        while (currentNode) {
+            if (currentNode.value === searchValue)
+                return currentNode.value;
+            currentNode = currentNode.next;
+        }
+
+    }
+    // Assignment Index Of
+    // list 3 40 3 7
+    // indexOf(3) >  0, 2
+    indexOf(value) {
+        let output = [], index = 0, currentNode = this.head;
+        while (currentNode) {
+
+            if (currentNode.value === value)
+                output.push(index);
+            index++;
+            currentNode = currentNode.next;
+        }
+        return output;
+    }
+}
+class Node {
+    constructor(value, next, prev) {
+        this.value = value;
+        this.next = next;
+        this.prev = prev;
+    }
 }
 
 
+
+
+
+
+exports ;
 // Big O notations
 const LL = new LinkedList();
 LL.addToHead(100);
