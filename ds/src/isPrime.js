7c1a79efe4a924158a58521b4699ef54bca1e4f6
Kiren <kiren@azurethailand.com>
2020-05-26 14:24:35 +0700

    is prime interview q updated

---
+++ ds/src/isPrime.js
@@ -0,0 +1,28 @@
+
+
+function getPrimeNumbersBetween(limit) {
+    let primeNumbers = [];
+    let counter = 0
+    const isPrime = (val) => {
+        if (val === 2) {
+            counter++;
+            return true;
+        }
+        let ll = Math.floor(val / 2);
+        for (let i = 2; i <= ll; i++) {
+            counter++;
+            if (val % i === 0) return false;
+        }
+        return true;
+    }
+    for (let i = 2; i <= limit; i++) {
+        counter++;
+        if (isPrime(i)) {
+            console.log(i)
+            primeNumbers.push(i);
+        }
+    }
+    return   primeNumbers;
+}
+
+console.log(getPrimeNumbersBetween(1000));
