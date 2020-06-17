554e712168a90d5f5b975b206f7c90baef5d715a
Kiren <kiren@azurethailand.com>
2020-06-17 09:52:24 +0700

    string comaprison

--- ds/src/CodeWars/stringsMatching.js
+++ ds/src/CodeWars/stringsMatching.js
@@ -12,32 +12,32 @@ function worries(str) {
   setOfStrings.forEach(a => { console.log(a); if (str === a) isTrue = true; })
   return isTrue ? "Stop worrying, everything will be all right!" : "Do you really care?"
 }
-// function worries(str) {
-//   const input = [
-//     "Making last-minute changes",
-//     "Sitting in a dark room and shaking",
-//     "Crying",
-//     "Laughing hysterically",
-//     "Not eating"
-//   ];
+function worries(str) {
+  const input = [
+    "Making last-minute changes",
+    "Sitting in a dark room and shaking",
+    "Crying",
+    "Laughing hysterically",
+    "Not eating"
+  ];
 
-//   return input.includes(str)
-//     ? "Stop worrying, everything will be all right!"
-//     : "Do you really care?";
-// }
-// function worries(str) {
-//   switch (str) {
-//     case 'Making last-minute changes':
-//     case 'Sitting in a dark room and shaking':
-//     case 'Crying':
-//     case 'Laughing hysterically':
-//     case 'Not eating':
-//       return 'Stop worrying, everything will be all right!'
-//       break
-//     default:
-//       return 'Do you really care?'
-//   }
+  return input.includes(str)
+    ? "Stop worrying, everything will be all right!"
+    : "Do you really care?";
+}
+function worries(str) {
+  switch (str) {
+    case 'Making last-minute changes':
+    case 'Sitting in a dark room and shaking':
+    case 'Crying':
+    case 'Laughing hysterically':
+    case 'Not eating':
+      return 'Stop worrying, everything will be all right!'
+      break
+    default:
+      return 'Do you really care?'
+  }
 
-// }
+}
 
 console.log(worries('sss'));
