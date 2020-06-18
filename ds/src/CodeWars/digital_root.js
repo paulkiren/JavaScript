8e2520ff7ed57c13d62ed609e6348fd1c946927c
Kiren <kiren@azurethailand.com>
2020-06-18 09:29:38 +0700

    recursive mode

--- ds/src/CodeWars/digital_root.js
+++ ds/src/CodeWars/digital_root.js
@@ -1,13 +1,21 @@
+// function digital_root(n) {
+//     // ...
+//     const sum = (value) => parseInt(value.toString().split('').reduce((a, b) => parseInt(a, 10) + parseInt(b, 10), 0), 10);
+
+//     let myResult = n
+
+//     while (myResult > 10) {
+//         myResult = sum(myResult)
+//     }
+//     return myResult;
+// }
+
+
+// recursive Mode
+
 function digital_root(n) {
-    // ...
-    const sum = (value) => parseInt(value.toString().split('').reduce((a, b) => parseInt(a, 10) + parseInt(b, 10), 0), 10);
-
-    let myResult = n
-
-    while (myResult > 10) {
-        myResult = sum(myResult)
-    }
-    return myResult;
+    if (n < 10) return n;
+   return  digital_root(n.toString().split('').reduce((a, b) =>a+ +b, 0));
 }
 
-console.log(digital_root(456));
+console.log(digital_root(654));
